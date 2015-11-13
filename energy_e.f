e3f878b8d7fc62149302c4d0f1c82895de6b1363
vessokolev <vesso.kolev@gmail.com>
2015-11-13 10:55:10 -0800

    Constants added

--- energy_e.f
+++ energy_e.f
@@ -24,22 +24,31 @@ c============================================================================
 
 chu      [classical bond energy for protein]
          call bondp(nbp,ibtp,jbtp,icbp,unit,ebondp,.true.,1)
+         print *,"ebondp",ebondp
 
 chu      [classical angle energy for protein]
          call thetap(ntp,ittp,jttp,kttp,ictp,unit,ethetp,1,.true.)
+         print *,"ethetp",ethetp
 
 chu      [classical torsion energy for protein]
          l_t_it=.true.
          call  phip(nphi,ipt,jpt,kpt,lpt,icp,cpn,cpd,cp,unit,ephi,.true.)
          call phipsi(ene)
          ephi=ephi+ene
+         print *,"ephi",ephi
 
 chu      [classical improper torsion energy for protein]
          l_t_it=.false.
          call phip(nitor,iit,jit,kit,lit,ici,cpni,cpdi,cpi,unit,eitor,.true.)
+         print *,"itor",eitor
 
 chu      [nonbonded electrostatic and VDW energy between protein atoms]
          call nonbond(lnon,inon,jnon,iac,crg,unit,evdwp,emumup,ehbpp,1,.true.)
+         print *,"nb",evdwp,emumup,ehbpp
+
+         call simple_vdw
+
+         print *,"simple vdw",sim_vdw
 
          E_tot=ebondp+ethetp+ephi+eitor+evdwp+emumup+ehbpp
 
